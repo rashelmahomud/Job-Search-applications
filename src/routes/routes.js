c3d17bba4a5cc8b5dd74a6cdd6cbeb1e683368c8
MCTC <rashelmahmudraj1998@gmail.com>
2022-12-26 00:10:16 -0800

    added some work setup

---
+++ src/routes/routes.js
@@ -0,0 +1,60 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../layout/dashboard/Dashboard";
+import Main from "../layout/main/Main";
+import Home from "../pages/home/Home";
+import JobDetails from "../pages/JobDetails";
+import Jobs from "../pages/Jobs";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/jobs",
+        element: <Jobs />,
+      },
+      {
+        path: "/job-details",
+        element: <JobDetails />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    //   {
+    //     path: "/register",
+    //     element: (
+    //       <PrivateRoute>
+    //         <AccountCreator />
+    //       </PrivateRoute>
+    //     ),
+    //   },
+    //   {
+    //     path: "/register/:type",
+    //     element: (
+    //       <PrivateRoute>
+    //         <AccountCreator />
+    //       </PrivateRoute>
+    //     ),
+    //   },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+
+export default routes;

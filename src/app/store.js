51c83518a94088808aa5060e9994ed1e370491eb
MCTC <rashelmahmudraj1998@gmail.com>
2022-12-27 01:52:06 -0800

    added midealware

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import apiSlice from '../features/api/apiSlice';
 import authSlice from '../features/auth/authSlice';
 
 export const store = configureStore({
+  [apiSlice.reducerPath]: apiSlice.reducer,
   reducer: {
 
     auth: authSlice,
+  },
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
---
+++ src/features/auth/authApi.js
@@ -0,0 +1,18 @@
+import apiSlice from "../api/apiSlice";
+
+
+
+const authApi = apiSlice.injectEndpoints({
+
+    endpoints: (builder) => ({
+        register: builder.mutation({
+            query: (data) => ({
+                method: 'POST',
+                url: '/user',
+                body: data,
+            })
+        })
+
+    }),
+
+});

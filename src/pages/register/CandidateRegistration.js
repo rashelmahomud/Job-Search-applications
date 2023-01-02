4f57d55c63d0d3f7c1ffc216e6dc28ff0d9ada20
MCTC <rashelmahmudraj1998@gmail.com>
2023-01-02 14:16:04 -0800

    addeds some fiends

--- src/pages/register/CandidateRegistration.js
+++ src/pages/register/CandidateRegistration.js
@@ -3,10 +3,12 @@ import { useForm, useWatch } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa";
 import { useRegisterMutation } from "../../features/auth/authApi";
+import { useSelector } from "react-redux";
 
 const CandidateRegistration = () => {
   const [countries, setCountries] = useState([]);
-  const { handleSubmit, register, control } = useForm();
+  const {user: {email}} = useSelector((state) => state.auth);
+  const { handleSubmit, register, control } = useForm({ defaultValues: {email}});
   const term = useWatch({ control, name: "term" });
   console.log(term);
   const navigate = useNavigate();
@@ -56,7 +58,7 @@ const CandidateRegistration = () => {
             <label className='mb-2' htmlFor='email'>
               Email
             </label>
-            <input type='email' id='email' {...register("email")} />
+            <input disabled type='email' id='email' {...register("email")} />
           </div>
           <div className='flex flex-col w-full max-w-xs'>
             <h1 className='mb-3'>Gender</h1>
